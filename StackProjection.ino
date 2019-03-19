b91be4d7e01584e902e5921b268a3c3f7b0e2974
Arthur Wang <arthraim@gmail.com>
2019-03-19 21:23:29 +0800

    draw cube in u8g2 way

--- StackProjection.ino
+++ StackProjection.ino
@@ -4,10 +4,11 @@
 
 U8G2_SH1107_64X128_F_4W_HW_SPI u8g2(U8G2_R0,14, /* dc=*/ 27, /* reset=*/ 33);
 
+#define UPDATE_DELAY  0
 #define SCALE 1
+#define STEP 0.5
 
 float roll, pitch, yaw;
-float scale = SCALE;
 
 Transform cubeTrans;
 
@@ -43,23 +44,36 @@ line3 cubeLines[] = {
 Camera cam(64, 128);
 
 void setup() {
-  u8g2.begin();
-}
+    u8g2.begin();
+
+    // Camera starts out at origin, looking along world +Y axis (its own +Z axis).
+    // Set camera back a few units so cube will be in view.
+    cam.transform.y = -5;}
 
 void loop() {
+    u8g2.clearBuffer();
+
     updateCube();
     drawCube();
+
+    u8g2.sendBuffer();
+    delay(UPDATE_DELAY);
 }
 
 void updateCube() {
     // Update cube transform
-    cubeTrans = Transform(roll += 2, pitch, yaw += 2, scale, scale, scale, 0, 0, 0);
+    cubeTrans = Transform(roll += STEP, pitch, yaw += STEP, SCALE, SCALE, SCALE, 0, 0, 0);
 }
 
 void drawCube() {
-  u8g2.clearBuffer();					// clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
-  u8g2.drawStr(0, 10, "Hello World!");	// write something to the internal memory
-  u8g2.sendBuffer();					// transfer internal memory to the display
-  delay(1000);
+    for (byte i = 0; i < 12; i++) {
+        // Project line to screen after applying cube transform
+        line2 line = cam.project(cubeTrans * cubeLines[i]);
+
+        // Draw if not clipped completely
+        if (!isnan(line.p0.x))
+        {
+            u8g2.drawLine(line.p0.x, line.p0.y, line.p1.x, line.p1.y);
+        }
+    }
 }
