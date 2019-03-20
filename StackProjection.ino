37d9070078cc562ff38969ec44a7099126f59443
Arthur Wang <arthraim@gmail.com>
2019-03-20 15:05:24 +0800

    update cube with acc data

--- StackProjection.ino
+++ StackProjection.ino
@@ -47,6 +47,8 @@ Camera cam(64, 128);
 
 // MPU9250
 MPU9250 IMU;
+int ax, ay, az;
+int deltaX, deltaY, deltaZ;
 
 void setup() {
     u8g2.begin();
@@ -64,13 +66,12 @@ void setup() {
 
 void loop() {
     u8g2.clearBuffer();
-
-    updateCube();
-    drawCube();
-
     u8g2.setFont(u8g2_font_6x12_tr);
     u8g2.drawStr(0, 7, "@ArthurWXY");
-    showGyroState();
+
+    updateAccelerometerState();
+    updateCube();
+    drawCube();
 
     u8g2.sendBuffer();
     delay(UPDATE_DELAY);
@@ -78,7 +79,13 @@ void loop() {
 
 void updateCube() {
     // Update cube transform
-    cubeTrans = Transform(roll += STEP, pitch, yaw += STEP, SCALE, SCALE, SCALE, 0, 0, 0);
+    cubeTrans = Transform(
+        roll -= (deltaY / 100), // += (deltaX / 100),
+        pitch, // += (deltaY / 100),
+        yaw += (deltaX / 100), // += (deltaZ / 100),
+        SCALE, SCALE, SCALE,
+        0, 0, 0
+        );
 }
 
 void drawCube() {
@@ -93,7 +100,7 @@ void drawCube() {
     }
 }
 
-void showGyroState() {
+void updateAccelerometerState() {
     // If intPin goes high, all data registers have new data
     // On interrupt, check if data ready interrupt
     if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {
@@ -104,58 +111,26 @@ void showGyroState() {
         IMU.ay = (float)IMU.accelCount[1] * IMU.aRes - IMU.accelBias[1];
         IMU.az = (float)IMU.accelCount[2] * IMU.aRes - IMU.accelBias[2];
 
-        IMU.readGyroData(IMU.gyroCount);  // Read the x/y/z adc values
-        IMU.getGres();
-
-        // Calculate the gyro value into actual degrees per second
-        // This depends on scale being set
-        IMU.gx = (float)IMU.gyroCount[0] * IMU.gRes;
-        IMU.gy = (float)IMU.gyroCount[1] * IMU.gRes;
-        IMU.gz = (float)IMU.gyroCount[2] * IMU.gRes;
-
-        IMU.readMagData(IMU.magCount);  // Read the x/y/z adc values
-        IMU.getMres();
-        // User environmental x-axis correction in milliGauss, should be
-        // automatically calculated
-        //IMU.magbias[0] = +470.;
-        // User environmental x-axis correction in milliGauss TODO axis??
-        //IMU.magbias[1] = +120.;
-        // User environmental x-axis correction in milliGauss
-        //IMU.magbias[2] = +125.;
-
-        // Calculate the magnetometer values in milliGauss
-        // Include factory calibration per data sheet and user environmental
-        // corrections
-        // Get actual magnetometer value, this depends on scale being set
-        IMU.mx = (float)IMU.magCount[0] * IMU.mRes * IMU.magCalibration[0] -
-                IMU.magbias[0];
-        IMU.my = (float)IMU.magCount[1] * IMU.mRes * IMU.magCalibration[1] -
-                IMU.magbias[1];
-        IMU.mz = (float)IMU.magCount[2] * IMU.mRes * IMU.magCalibration[2] -
-                IMU.magbias[2];
-
-        IMU.tempCount = IMU.readTempData();  // Read the adc values
-        // Temperature in degrees Centigrade
-        IMU.temperature = ((float) IMU.tempCount) / 333.87 + 21.0;
-
         // Accelerometer in mg
-        int ax = (int)(1000 * IMU.ax);
-        int ay = (int)(1000 * IMU.ay);
-        int az = (int)(1000 * IMU.az);
+        if (ax == 0 || ay == 0 || az == 0) {
+            ax = (int)(1000 * IMU.ax);
+            ay = (int)(1000 * IMU.ay);
+            az = (int)(1000 * IMU.az);
+        } else {
+            deltaX = (int)(1000 * IMU.ax) - ax;
+            deltaY = (int)(1000 * IMU.ay) - ay;
+            deltaZ = (int)(1000 * IMU.az) - az;
+        }
 
-        // Gyroscope in o/s
-        int gx = (int)(IMU.gx);
-        int gy = (int)(IMU.gy);
-        int gz = (int)(IMU.gz);
+        drawXYZ(deltaX, deltaY, deltaZ);
 
-        // Magnetometer in mG
-        int mx = (int)(IMU.mx);
-        int my = (int)(IMU.my);
-        int mz = (int)(IMU.mz);
-
-        drawXYZ(ax, ay, az);
-        // drawXYZ(gx, gy, gz);
-        // drawXYZ(mx, my, mz);
+        char buffer[20];
+        itoa(ax, buffer,10);
+        u8g2.drawStr(35, 128-2*7, buffer);
+        itoa(ay, buffer,10);
+        u8g2.drawStr(35, 128-1*7, buffer);
+        itoa(az, buffer,10);
+        u8g2.drawStr(35, 128-0*7, buffer);
     }
 }
 
